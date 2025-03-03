a87ebd72c41544c323e30e123b7b0c3e98a5aa9b
YenilmezKediB01 <littlecatece@gmail.com>
2025-03-03 20:55:46 +0300

    Create katze.py
    kod

---
+++ katze.py
@@ -0,0 +1,17 @@
+meme_dict = {
+    "CRINGE": "Garip ya da utandırıcı bir şey"
+    "LOL": "Komik bir şeye verilen cevap"
+    "ROFL":"Bir şakaya karşılık cevap"
+    "SHEESH":"Onaylamak"
+    "CREEPY":"korkunç"
+    "GG":"İyi Oyun"
+    "EZ":"Kolay/kolaydı"
+            }
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print(meme_dict[word])
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("Böyle bir kelime yok ki")
